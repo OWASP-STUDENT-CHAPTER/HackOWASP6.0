b7667b779ffbeecd0a82e51eaf1ee40624cf35f4
Siddharth-Agg-arwal <aggarwal.siddharth2003@outlook.com>
2024-04-14 21:24:59 +0530

    devfolio button

--- frontend/src/components/landingpage/Landingcontent.jsx
+++ frontend/src/components/landingpage/Landingcontent.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
+import { useEffect } from 'react';
+
 import styles from './landingcontent.module.css';
 import folderopen from '../../assets/landingpage/folderopen.png'
 import floppy from '../../assets/landingpage/floppy.png'
 import folder from '../../assets/landingpage/folder.png'
 import sand from '../../assets/landingpage/sand.png'
 import computer from '../../assets/landingpage/computer.png'
+import devfolio from '../../assets/landingpage/01.png'
 
 
 const Landingcontent = () => {
+  
+  React.useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://apply.devfolio.co/v2/sdk.js';
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+  
   return (
     <div className={styles.landingcontent}>
       <div className={styles.content}>
       <div className={styles.hackowasptitle}>HackOWASP<br />6.0</div>
+      <div 
+        class="apply-button" 
+        className={styles.apply_button}
+        data-hackathon-slug="YOUR-HACKATHON-SLUG" 
+        data-button-theme="light"
+        // style="height: 44px; width: 312px"
+      ></div>
       <div className={styles.animated_icons}>
         <div className={styles.icon1}>
           <img
