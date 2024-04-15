aa0982e9bc386f7e0ad8d344b391f2d5ae09952e
Siddharth-Agg-arwal <aggarwal.siddharth2003@outlook.com>
2024-04-15 22:01:29 +0530

    added scroll navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 // import logo from './logo.svg';
 import styles from './App.module.css';
-// import MySVGComponent from './pages/Landingpage/landingpage2';
-import Navbar from './components/Navbar';
 import ScrollSpyNavigation from './components/ScrollNavigation/ScrollNavigation';
-import AboutUs from './pages/AboutusPage/aboutus';
-import LandingPage from './pages/Landingpage/landingpage';
 import Footer from './components/footer/Footer';
 // import { Link } from "react-scroll";
 
 function App() {
   return (
     <div className={styles.App}>
-<<<<<<< HEAD
       <ScrollSpyNavigation />
-=======
-      <Navbar />
-      <LandingPage id="landingpage" />
-      <AboutUs id="aboutus"/>
       <Footer />
->>>>>>> 2f6a37608c400cf15d664a67aa6dd72eda5ed28b
     </div>
   );
 }
