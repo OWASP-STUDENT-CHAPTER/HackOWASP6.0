e5eed7c3812ae684900f9f0c9138341a263331da
its-me-ojas <crestfallenwizard@gmail.com>
2024-04-16 00:57:52 +0530

    navbar set for now (hamburger will be set for later)

--- src/components/ScrollNavigation/ScrollNavigation.jsx
+++ src/components/ScrollNavigation/ScrollNavigation.jsx
@@ -6,60 +6,60 @@ import TracksPage from '../../pages/TracksPage/trackspage';
 
 function ScrollSpyNavigation() {
     useEffect(() => {
-    const sections = document.querySelectorAll("section[id]");
+        const sections = document.querySelectorAll("section[id]");
 
-    function navHighlighter() {
-    let scrollY = window.pageYOffset;
+        function navHighlighter() {
+            let scrollY = window.pageYOffset;
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        const sectionId = current.getAttribute("id");
-        
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector(`.navigation a[href*=${sectionId}]`).classList.add("active");
-        } else {
-            document.querySelector(`.navigation a[href*=${sectionId}]`).classList.remove("active");
+            sections.forEach(current => {
+                const sectionHeight = current.offsetHeight;
+                const sectionTop = current.offsetTop - 50;
+                const sectionId = current.getAttribute("id");
+
+                if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                    document.querySelector(`.navigation a[href*=${sectionId}]`).classList.add("active");
+                } else {
+                    document.querySelector(`.navigation a[href*=${sectionId}]`).classList.remove("active");
+                }
+            });
         }
-    });
-    }
 
-    window.addEventListener("scroll", navHighlighter);
-    
-    return () => {
-        window.removeEventListener("scroll", navHighlighter);
-    };
-}, []);
+        window.addEventListener("scroll", navHighlighter);
+
+        return () => {
+            window.removeEventListener("scroll", navHighlighter);
+        };
+    }, []);
 
     return (
-    <div>
-        <div className="side-bar">
-        <div className="navigation">
-            <div className="home"><a href="#hackowasp6">HackOWASP 6</a></div>
-            <div><a href="#about">About</a></div>
-            <div><a href="#services">Tracks</a></div>
-            <div><a href="#timeline">Timeline</a></div>
-            <div><a href="#sponsors">Sponsors</a></div>
+        <div>
+            <div className="side-bar">
+                <div className="navigation">
+                    <div className="home"><a href="#hackowasp6">HackOWASP 6</a></div>
+                    <div className='linksOnNavbar'><a href="#about">About</a></div>
+                    <div className='linksOnNavbar'><a href="#services">Tracks</a></div>
+                    <div className='linksOnNavbar'> <a href="#timeline">Timeline</a></div>
+                    <div className='linksOnNavbar'><a href="#sponsors">Sponsors</a></div>
+                </div>
+            </div>
+            <div className="main-content">
+                <section id="hackowasp6">
+                    <LandingPage />
+                </section>
+                <section id="about">
+                    <AboutUs />
+                </section>
+                <section id="services">
+                    {/* <TracksPage /> */}
+                </section>
+                <section id="timeline">
+                    {/* <h1>timeline</h1> */}
+                </section>
+                <section id="sponsors">
+                    {/* <h1>sponsors</h1> */}
+                </section>
+            </div>
         </div>
-        </div>
-        <div className="main-content">
-        <section id="hackowasp6">
-            <LandingPage />
-        </section>
-        <section id="about">
-            <AboutUs />
-        </section>     
-        <section id="services">
-            {/* <TracksPage /> */}
-        </section>
-        <section id="timeline">
-            {/* <h1>timeline</h1> */}
-        </section>
-        <section id="sponsors">
-            {/* <h1>sponsors</h1> */}
-        </section>
-        </div>
-    </div>
     );
 }
 
