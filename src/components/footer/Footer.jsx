4a2c2177c2731b5fb3921201634bfc6b24e81ff9
Parth-11 <parth12.mgg@gmail.com>
2024-04-15 12:22:52 +0530

    Footer ver1

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import styles from '../footer/Footer.module.css';
+import email from '../../assets/footer/Group 12.png';
+import instagram from '../../assets/footer/instagram.png';
+import linkedin from '../../assets/footer/linkedin.png';
+import discord from '../../assets/footer/discord.png';
+import twitter from '../../assets/footer/twitter.png';
 
 const Footer = () =>{
     return <footer>
@@ -8,15 +13,20 @@ const Footer = () =>{
                 <h1>
                     OWASP
                 </h1>
-                <ul>
-
+                <ul className={styles.links}>
+                    <li><a href="instagram.com/owasp_tiet"><img src={instagram} alt="instagram" /></a></li>
+                    <li><a href=""></a><img src={linkedin} alt="linkedin" /></li>
+                    <li><a href=""><img src={discord} alt="" /></a></li>
+                    <li><a href=""><img src={twitter} alt="" /></a></li>
                 </ul>
             </div>
             <div className={styles.contact}>
                 <h1>
                     GET IN TOUCH
                 </h1>
-                <span></span>
+                <a href="mailto:owasptu@gmail.com" className={styles.email}>
+                    <img src={email} alt="" />
+                    </a>
             </div>
         </div>
     </footer>
