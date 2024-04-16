2d94b22a99f5ab36da7370404dd43dc0d53bb553
MJ-NJ <mjha_be21@thapar.edu>
2024-04-17 01:52:48 +0530

    fix

--- src/components/ScrollNavigation/ScrollNavigation.jsx
+++ src/components/ScrollNavigation/ScrollNavigation.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import './hamburger.css'
+import './hamburger.css';
 import AboutUs from '../../pages/AboutusPage/aboutus';
 import LandingPage from '../../pages/Landingpage/landingpage';
 import Sponsorpage from '../../pages/SponsorPage/sponsorpage';
@@ -7,73 +7,101 @@ import TracksPage from '../../pages/TracksPage/trackspage';
 import ham from './../../../src/assets/navbar/hamburger.png';
 
 function ScrollSpyNavigation() {
-    useEffect(() => {
-        const sections = document.querySelectorAll("section[id]");
+	useEffect(() => {
+		const sections = document.querySelectorAll('section[id]');
 
-        function navHighlighter() {
-            let scrollY = window.pageYOffset;
+		function navHighlighter() {
+			let scrollY = window.pageYOffset;
 
-            sections.forEach(current => {
-                const sectionHeight = current.offsetHeight;
-                const sectionTop = current.offsetTop - 50;
-                const sectionId = current.getAttribute("id");
+			sections.forEach((current) => {
+				const sectionHeight = current.offsetHeight;
+				const sectionTop = current.offsetTop - 50;
+				const sectionId = current.getAttribute('id');
 
-                if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-                    document.querySelector(`.navigation a[href*=${sectionId}]`).classList.add("active");
-                } else {
-                    document.querySelector(`.navigation a[href*=${sectionId}]`).classList.remove("active");
-                }
-            });
-        }
+				if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+					document
+						.querySelector(`.navigation a[href*=${sectionId}]`)
+						.classList.add('active');
+				} else {
+					document
+						.querySelector(`.navigation a[href*=${sectionId}]`)
+						.classList.remove('active');
+				}
+			});
+		}
 
-        window.addEventListener("scroll", navHighlighter);
+		window.addEventListener('scroll', navHighlighter);
 
-        return () => {
-            window.removeEventListener("scroll", navHighlighter);
-        };
-    }, []);
+		return () => {
+			window.removeEventListener('scroll', navHighlighter);
+		};
+	}, []);
 
-    const hamburgerHanlder = () => {
-        const hamPopup = document.querySelector('.ham-popup');
-        hamPopup.style.display = hamPopup.style.display === 'none' ? 'flex' : 'none';
-    }
-    return (
-        <div>
-            <div className="side-bar">
-                <div className="navigation">
-                    <div className="home"><a href="#hackowasp6">HackOWASP 6</a></div>
-                    <div className='linksOnNavbar'><a href="#about">About</a></div>
-                    <div className='linksOnNavbar'><a href="#services">Tracks</a></div>
-                    <div className='linksOnNavbar'> <a href="#timeline">Timeline</a></div>
-                    <div className='linksOnNavbar'><a href="#sponsors">Sponsors</a></div>
-                    <div className='ham'><img src={ham} alt="hamburger" height={40} onClick={hamburgerHanlder} /></div>
-                </div>
-                <div className='ham-popup navigation'>
-                    <div className='ham-icons'><a href="#about">About</a></div>
-                    <div className='ham-icons'><a href="#services">Tracks</a></div>
-                    <div className='ham-icons'> <a href="#timeline">Timeline</a></div>
-                    <div className='ham-icons'><a href="#sponsors">Sponsors</a></div>
-                </div>
-            </div>
-            <div className="main-content">
-                <section id="hackowasp6">
-                    <LandingPage />
-                </section>
-                <section id="about">
-                    <AboutUs />
-                </section>
-                <section id="services">
-                    {/* <TracksPage /> */}
-                </section>
-                <section id="timeline">
-                    {/* <h1>timeline</h1> */}
-                </section>
-                <section id="sponsors">
-                    <Sponsorpage />
-                </section>
-            </div>
-        </div>
-    );
+	const hamburgerHanlder = () => {
+		const hamPopup = document.querySelector('.ham-popup');
+		hamPopup.style.display =
+			hamPopup.style.display === 'none' ? 'flex' : 'none';
+	};
+	return (
+		<div>
+			<div className="side-bar">
+				<div className="navigation">
+					<div className="home">
+						<a href="#hackowasp6">HackOWASP 6</a>
+					</div>
+					<div className="linksOnNavbar">
+						<a href="#about">About</a>
+					</div>
+					<div className="linksOnNavbar">
+						<a href="#services">Tracks</a>
+					</div>
+					<div className="linksOnNavbar">
+						{' '}
+						<a href="#timeline">Timeline</a>
+					</div>
+					<div className="linksOnNavbar">
+						<a href="#sponsors">Sponsors</a>
+					</div>
+					<div className="ham">
+						<img
+							src={ham}
+							alt="hamburger"
+							height={40}
+							onClick={hamburgerHanlder}
+						/>
+					</div>
+				</div>
+				<div className="ham-popup navigation">
+					<div className="ham-icons">
+						<a href="#about">About</a>
+					</div>
+					<div className="ham-icons">
+						<a href="#services">Tracks</a>
+					</div>
+					<div className="ham-icons">
+						{' '}
+						<a href="#timeline">Timeline</a>
+					</div>
+					<div className="ham-icons">
+						<a href="#sponsors">Sponsors</a>
+					</div>
+				</div>
+			</div>
+			<div className="main-content">
+				<section id="hackowasp6">
+					<LandingPage />
+				</section>
+				<section id="about">
+					<AboutUs />
+				</section>
+				<section id="services">{/* <TracksPage /> */}</section>
+				<section id="timeline">{/* <h1>timeline</h1> */}</section>
+				<section id="sponsors">
+					<Sponsorpage />
+				</section>
+			</div>
+		</div>
+	);
 }
 
 export default ScrollSpyNavigation;
--- src/pages/TracksPage/trackspage.jsx
+++ src/pages/TracksPage/trackspage.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from 'react';
 import styles from './trackspage.module.css';
 import TracksCard from '../../components/trackspage/tracks-card';
 
 const TracksPage = () => {
-  return (
-    <div className={styles.trackspage}>
-      <div className={styles.tracks_heading}>Tracks</div>
-      <div>
-        <TracksCard />
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className={styles.trackspage}>
+			<div className={styles.tracks_heading}>Tracks</div>
+			<div>{/* <TracksCard /> */}</div>
+		</div>
+	);
+};
 
-export default TracksPage
+export default TracksPage;
