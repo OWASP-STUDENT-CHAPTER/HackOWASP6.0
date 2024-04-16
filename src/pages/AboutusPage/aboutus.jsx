6e163db9590aa89c7596ab4aaa3811f04bc877fa
viren1610 <virenplays@gmail.com>
2024-04-16 17:58:52 +0530

    typo

--- src/pages/AboutusPage/aboutus.jsx
+++ src/pages/AboutusPage/aboutus.jsx
@@ -11,7 +11,7 @@ const AboutUs = () => {
                 <div className={styles.minicard1}>2000+ Participation</div>
             </div>
             <div className={styles.card2}>
-                <AboutUsCard heading="OWASP" content="Thapar Institute of Engineering and Technology (TIET) has been a steady source of highly skilled talent to the nation as well as overseas. A pioneer in engineering education, research and innovation. The team of OWASP Student Chapter, one of the gilt-edged coding society, involves ingenious mind solvers who are eager to make the world a better place to live in with their innovative techniques and discoveries. By successfully organizing many hackathons, tech-talks, workshops and coding nights, we have always strived hard to maintain the coding culture throughout the campus."/>
+                <AboutUsCard heading="OWASP" content="Thapar Institute of Engineering and Technology (TIET) has been a steady source of highly skilled talent to the nation as well as overseas. A pioneer in engineering education, research and innovation. The team of OWASP Student Chapter, one of the gilt-edged coding society, involves ingenious mind solvers who are eager to make the world a better place to live in with their innovative techniques and discoveries. By successfully organizing many hackathons, tech-talks, workshops and coding nights, we have always strived hard to maintain the coding culture throughout the campus !!."/>
                 <div className={styles.minicard2}>36 Hours</div>
             </div>
         </div>
